b40b9a2f048ad5f017455902186313c5d4351dc2
Carlos Carballo <159819105+Metaler127@users.noreply.github.com>
2024-06-14 02:20:28 -0700

    aaa cambios
    aa

--- routes/api.php
+++ routes/api.php
@@ -9,19 +9,19 @@ use App\Http\Controllers\api\aprobacionesController;
 
 
 Route::post("/registrar", [usuarioController::class,"registrar"]);
-Route::post("/Login", [usuarioController::class,"Login"]);
+Route::post("/login", [usuarioController::class,"Login"]);
 Route::get("/user", [usuarioController::class,"getUserData"])->middleware("auth:sanctum");
 Route::get("/planta", [plantaController::class,"index"]);
 Route::post("/planta", [plantaController::class,"store"]);
-Route::post("/Observacion", [observacionesController::class,"store"])->middleware("auth:sanctum");
-Route::get("/Observacion", [observacionesController::class,"index"]);
-Route::get("/Observacion/{id}", [ObservacionesController::class,"show"]);
-Route::patch("/Observacion/{id}", [observacionesController::class,"update"]);
-Route::delete("/Observacion/{id}", [observacionesController::class,"destroy"]);
-Route::get("/Aprobaciones", [aprobacionesController::class,"index"]);
-Route::post("/Aprobaciones", [aprobacionesController::class,"store"]);
-Route::patch("/Aprobaciones/{id}", [aprobacioesController::class,"update"]);
-Route::delete("Aprobaciones/{id}", [aprobacioesController::class,"destroy"]);
+Route::post("/observacion", [observacionesController::class,"store"])->middleware("auth:sanctum");
+Route::get("/observacion", [observacionesController::class,"index"]);
+Route::get("/observacion/{id}", [ObservacionesController::class,"show"]);
+Route::patch("/observacion/{id}", [observacionesController::class,"update"]);
+Route::delete("/observacion/{id}", [observacionesController::class,"destroy"]);
+Route::get("/aprobaciones", [aprobacionesController::class,"index"]);
+Route::post("/aprobaciones", [aprobacionesController::class,"store"]);
+Route::patch("/aprobaciones/{id}", [aprobacioesController::class,"update"]);
+Route::delete("/aprobaciones/{id}", [aprobacioesController::class,"destroy"]);
 
 
 
